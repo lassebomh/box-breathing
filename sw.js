a677eafd27476dfae3b7a7e5d809fb74869bf6f5
Lasse <lasse@bomh.net>
2022-11-21 18:03:17 +0100

    Switch to cache first

--- sw.js
+++ sw.js
@@ -1,37 +1,31 @@
 
 const MAIN_CACHE = 'main';
 
-self.addEventListener('install', event => {
-    console.log("installing service worker");
-});
+// self.addEventListener('install', event => {
+//     console.log("installing service worker");
+// });
 
-self.addEventListener('activate', event => {
-    console.log("activating service worker");
-});
+// self.addEventListener('activate', event => {
+//     console.log("activating service worker");
+// });
 
-// Attempt the request and store and return successful responses. If it fails
-// look into the cache.
-const networkFirst = (event) => {
-    
+const cacheFirst = (event) => {
+
+    if (event.request.method.toLowerCase() != 'get') return
+
     const referrerOrigin = event.request.referrer && new URL(event.request.referrer).origin
     const requestOrigin = event.request.url && new URL(event.request.url).origin
 
-    if (referrerOrigin == requestOrigin) {
-        event.respondWith((async () => {
-            let cache = await caches.open(MAIN_CACHE)
-            try {
-                let response = await fetch(event.request)
-                cache.put(event.request, response.clone())
+    if (referrerOrigin != requestOrigin) return
 
-                return response
-            } catch (error) {
-                const cachedResponse = await caches.match(event.request);
-                if (cachedResponse) return cachedResponse;
+    event.respondWith((async ()=>{
+        const cache = await caches.open(MAIN_CACHE)
+        const cachedResponse = await caches.match(event.request)
 
-                throw error;
-            }
-        })());
-    }
+        fetch(event.request).then((response) => cache.put(event.request, response.clone()))
+
+        return cachedResponse
+    })())
 };
 
-self.addEventListener('fetch', networkFirst);
+self.addEventListener('fetch', cacheFirst);
